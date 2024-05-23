0cf8dfe45bd3e657ac0493f28b1d7d8cd79b7660
zhogoshi <hogoshi@yandex.ru>
2024-05-23 21:19:14 +0700

    minecrell remove, editorconfig bastard formatting removal, mappings converter, runserver and much other things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
-import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
 	java
 	id("com.github.johnrengelman.shadow") version "8.1.1"
-	id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+	id("io.papermc.paperweight.userdev").version("1.5.11")
+	id("io.github.patrick.remapper") version "1.4.1"
+	id("xyz.jpenilla.run-paper") version "2.2.3"
+	id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
 }
 
 // change this to your needs
 val mainClassName = "SamplePlugin"
 group = "dev.himirai.${mainClassName.lowercase()}"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.0"
 val internal = "$group.internal"
 
 repositories {
@@ -18,48 +21,61 @@ repositories {
 }
 
 dependencies {
-	compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+	paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
 	compileOnly("org.projectlombok:lombok:1.18.32")
 	annotationProcessor("org.projectlombok:lombok:1.18.32")
 	testImplementation(platform("org.junit:junit-bom:5.10.2"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("com.github.seeseemelk:MockBukkit-v1.19:3.0.0")
+	testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.86.1")
 }
 
 java {
 	toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
-bukkit {
+bukkitPluginYaml {
 	main = "$group.$mainClassName"
-	apiVersion = "1.19"
-	author = "Himirai"
+	apiVersion = "1.20"
+	authors.add("Himirai")
 	version = project.version.toString()
-	load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
-//    depend = listOf("WorldEdit")
-//    softDepend = listOf("Vault")
+	load = BukkitPluginYaml.PluginLoadOrder.POSTWORLD
+//    depend.add("WorldEdit")
+//    softDepend.add("Vault")
 }
 
 tasks {
+	runServer {
+		version("1.20.4")
+	}
+
+	shadowJar {
+		val relocations = listOf<String>()
+		relocations.forEach { relocate(it, "$internal.$it") }
+		archiveClassifier.set("")
+		archiveFileName.set("v${project.version}/$mainClassName.jar")
+	}
+
+	build {
+		dependsOn(shadowJar)
+	}
+
+	reobfJar {
+		outputJar.set(layout.buildDirectory.file("libs/v${project.version}/${project.name}-remapped.jar"))
+		doFirst {
+			val versionDir = file("${layout.buildDirectory}/libs/v${project.version}")
+			if (!versionDir.exists()) versionDir.mkdirs()
+		}
+	}
+
+	assemble {
+		dependsOn(reobfJar)
+	}
+
 	test {
 		useJUnitPlatform()
 		testLogging {
 			events("passed", "skipped", "failed")
 		}
-	}
-
-	shadowJar {
-//        If you need to relocate something, then, uncomment it and paste in list path to folders which should be relocated
-//        val relocations = listOf(
-//            "com"
-//        )
-//        relocations.forEach { relocate(it, "$internal.$it") }
-		archiveClassifier.set("")
-		archiveFileName.set("$mainClassName-${project.version}.jar")
-	}
-
-	build {
-		dependsOn(shadowJar)
 	}
 }
